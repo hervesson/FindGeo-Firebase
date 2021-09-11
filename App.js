f92607e5cc20170ffbc539bf16300069e39f0bc5
hervesson <hervessonporto@hotmail.com>
2021-09-11 18:58:50 -0300

    Gravar e procurar documentos

--- App.js
+++ App.js
@@ -1,112 +1,84 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import { View, Text, Button } from 'react-native';
+import { firebaseApp } from './FirebaseConfig';
+import firestore from '@react-native-firebase/firestore';
+import { geohashForLocation, geohashQueryBounds, distanceBetween } from "geofire-common";
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+const App = () => {
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+   const adicionar = () => {
+      // Compute the GeoHash for a lat/lng point
+      const lat = -2.6138411;
+      const lng = -44.2544649;
+      const hash = geohashForLocation([lat, lng]);
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+      // Add the hash and the lat/lng to the document. We will use the hash
+      // for queries and the lat/lng for distance comparisons.
+      const londonRef = firestore().collection('cities').doc('SLZ');
+      londonRef.update({
+         geohash: hash,
+         lat: lat,
+         lng: lng
+      }).then(() => {
+        console.warn('Deu certo')
+      });
+   }
 
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
+   const buscar = () => {
+      // Encontre cidades a 50 km de Londres
+      const center = [51.5074, 0.1278];
+      const radiusInM = 50 * 1000;
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+      // Cada item em 'limites' representa um par startAt / endAt. Temos que emitir
+      // uma consulta separada para cada par. Pode haver até 9 pares de limites
+      // dependendo da sobreposição, mas na maioria dos casos há 4.
+      const bounds = geohashQueryBounds(center, radiusInM);
+      const promises = [];
+      for (const b of bounds) {
+        const q = firestore().collection('cities')
+          .orderBy('geohash')
+          .startAt(b[0])
+          .endAt(b[1]);
 
-export default App;
+        promises.push(q.get());
+      }
+
+      // Colete todos os resultados da consulta em uma única lista
+      Promise.all(promises).then((snapshots) => {
+        const matchingDocs = [];
+
+        for (const snap of snapshots) {
+          for (const doc of snap.docs) {
+            const lat = doc.get('lat');
+            const lng = doc.get('lng');
+
+            // Temos que filtrar alguns falsos positivos devido ao GeoHash
+            // precisão, mas a maioria corresponderá
+            const distanceInKm = distanceBetween([lat, lng], center);
+            const distanceInM = distanceInKm * 1000;
+            if (distanceInM <= radiusInM) {
+              matchingDocs.push(doc);
+            }
+          }
+        }
+
+        return matchingDocs;
+      }).then((matchingDocs) => {
+        // Processa os documentos correspondentes
+        // ...
+        console.log(matchingDocs)
+      });
+   }
+
+   return (
+      <View>
+         <Text>Teste Find Geo</Text>
+         <Button title={'Adicionar'} onPress={() => adicionar()} color={'gray'} />
+          <Text>Buscar</Text>
+         <Button title={'Procurar'} onPress={() => buscar()} color={'#EDBC00'} />
+      </View>
+   )
+}
+
+export default App
